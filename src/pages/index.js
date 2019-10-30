000c2752f80062a7c5a28d47565f6fb7ac8d9f00
Frank Dinies <51444977+fdinies@users.noreply.github.com>
2019-10-30 15:50:07 +0100

    some minor style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,11 @@ export default () => {
       <h1>Hello</h1>
       <h2>I'm Frank, a full-stack developer.</h2>
       <p>
-        Need a delevoper ? &#128073;<Link to="/contact">Contact me</Link>
+        Need a delevoper ?
+        <span role="img" aria-label="contact">
+          &#128073;
+        </span>
+        <Link to="/contact">&nbsp;Contact me</Link>
       </p>
     </Layout>
   )
