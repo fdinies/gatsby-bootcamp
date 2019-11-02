612c3a9e008db99f48e4c29dc9c027ce3223d59b
Frank Dinies <51444977+fdinies@users.noreply.github.com>
2019-11-02 14:37:39 +0100

    changed index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default () => {
     <Layout>
       <Head title="Home" />
       <h1>Hello</h1>
-      <h2>I'm Frank, a full-stack developer.</h2>
+      <h2>I'm Frank, a full-stack developer :-)</h2>
       <p>
         Need a developer ?
         <span role="img" aria-label="contact">
